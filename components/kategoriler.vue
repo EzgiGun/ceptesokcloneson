99c79db825a4cf260582486ff0258612af7234c8
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2022-12-07 22:58:04 +0300

    Add files via upload

---
+++ components/kategoriler.vue
@@ -0,0 +1,277 @@
+<template>
+  <div>
+    <section class="section home-categories">
+      <div class="wrapper mobile-wide">
+        <h2 class="kategori_baslik">Kategoriler</h2>
+        <ul class="kategori_liste">
+          <li class="kategori_listesi">
+            <a class="" href="/kampanyalar-cl-95/"
+              ><h3 class="Kategori_baslik">Haftanın Fırsatları</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/banner/420x480/1cf71fc5050381932117b52fc12051d6.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="" href="/kargolu-firsat-urunleri-cl-492/"
+              ><div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/banner/420x480/e27f95cd9fd84b17d2f4980d71d9a22b.jpg');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a
+              class="link-to-pages-category"
+              href="/patates-sogan-sarimsak-c-1599"
+              ><h3 class="Kategori_baslik">Meyve &amp; Sebze</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/meyve-sebze@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/kirmizi-et-c-1252"
+              ><h3 class="Kategori_baslik">Et &amp; Tavuk &amp; Şarküteri</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/ettavukhindi@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/sut-c-1348"
+              ><h3 class="Kategori_baslik">Süt &amp; Süt Ürünleri</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/yogurtsuturunleri@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/yumurta-c-1357"
+              ><h3 class="Kategori_baslik">Kahvaltılık</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/kahvaltilik@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/ekmek-c-1447"
+              ><h3 class="Kategori_baslik">Ekmek &amp; Pastane</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/unlumamuller@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/dondurulmus-patates-c-1919"
+              ><h3 class="Kategori_baslik">Dondurulmuş Ürünler</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/dondurulmus@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/sivi-yaglar-c-1284"
+              ><h3 class="Kategori_baslik">Yemeklik Malzemeler</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/yemeklikmalzemeleri@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/cikolata-c-1466"
+              ><h3 class="Kategori_baslik">Atıştırmalık</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/atistirmalik@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/su-c-1440"
+              ><h3 class="Kategori_baslik">İçecek</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/icecek@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/sampuan-banyo-dus-c-1454"
+              ><h3 class="Kategori_baslik">Kişisel Bakım &amp; Kozmetik</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/kisiselbakim@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/bulasik-c-1442"
+              ><h3 class="Kategori_baslik">Temizlik</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/temizlik@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/bebek-bezi-c-1745"
+              ><h3 class="Kategori_baslik">Anne - Bebek &amp; Çocuk</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/bebekurunleri@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/tuvalet-kagidi-c-1959"
+              ><h3 class="Kategori_baslik">Kağıt Ürünleri</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/kagit@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/kus-yemi-c-1951"
+              ><h3 class="Kategori_baslik">Evcil Dostlar</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/evcilhayvan.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/ampul-aydinlatma-c-1575"
+              ><h3 class="Kategori_baslik">Elektronik</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/elektronik@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/kadin-ic-giyim-c-1943"
+              ><h3 class="Kategori_baslik">
+                Giyim &amp; Ayakkabı &amp; Aksesuar
+              </h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/giyim@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+          <li class="kategori_listesi">
+            <a class="link-to-pages-category" href="/ev-tekstili-c-1723"
+              ><h3 class="Kategori_baslik">Ev &amp; Yaşam</h3>
+              <div
+                class="image category-image imagewrap bg"
+                placeholder="/assets/images/placeholder/home-category.png"
+              >
+                <div
+                  class="imagewrap-image"
+                  style="background-image: url('https://cdnd-tr.ceptesok.com/category/216x240/evyasam@2x.png');"
+                ></div></div
+            ></a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style>
+ .section.home-categories .kategori_baslik {
+    font-size:1.4rem;
+    letter-spacing:-.025em;
+    color:rgba(51,41,10,.3);
+    margin-bottom:2.4rem
+   }
+   .section.home-categories .kategori_liste {
+    display:flex;
+    flex-flow:row wrap;
+    justify-content:flex-start;
+    align-items:flex-start;
+    margin:-1rem
+   }
+</style>
