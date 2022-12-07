837df7eeed6388e66154773e4429a94065c8a4c9
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2022-12-07 22:52:40 +0300

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <Navbar />   
+      <Slider2 />
+      <Products />
+      <Category />
+      <Kampanya />
+      <kampanyalar />
+      <haftaninfirsaatlari />
+      <Footer />
+    </div>
+  </template>
+
+
+  <script>
+  import Navbar from '../components/navbar.vue';
+  import Slider2 from '../components/slider2.vue';
+  import Footer from '../components/footer.vue';
+  import Products from '../components/urunler.vue';
+  import Kampanya from '../components/kampanya.vue';
+  import Category from '../components/kategoriler.vue';
+  import kampanyalar from '../components/kampanyalar.vue';
+  import haftaninfirsaatlari from '../components/haftaninfirsaatlari.vue';
+
+  export default {
+      name: "IndexPage",
+      components: { Navbar, Slider2, Footer, Category, Kampanya, Products, haftaninfirsaatlari, kampanyalar }
+  }
+  </script>
