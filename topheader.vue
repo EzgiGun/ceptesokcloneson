05729bffb4cc67f63d052897b0c2e26ad91e579d
Ezgi Gün <80538645+EzgiGun@users.noreply.github.com>
2022-12-07 22:52:22 +0300

    Add files via upload

---
+++ topheader.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="top-sidemenu">        
+         <a href="https://kurumsal.sokmarket.com.tr">Kurumsal</a>
+         <a href="https://sokmarketyatirimciiliskileri.com/tr">Yatırımcı İlişkileri</a>
+         
+       </div>
+</template>
+
+<style>
+ .top-sidemenu>a {
+    font-weight:500;
+    display:inline-block;
+    font-size:1.2rem;
+    border-radius:.4rem;
+    padding:.4rem .9rem;
+    text-transform:uppercase;
+    background-color:#05519f;
+    color:#fff;
+    transition:background-color .2s
+   }
+</style>
